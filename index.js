dae074da969140c28691f5030036dfbbf7aa2ad8
Moinak Banerjee <moinak878@gmail.com>
2021-05-23 20:04:50 +0530

    :rocket: uploaded index.js

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const jsonfile = require('jsonfile')
-const moment = require('moment')
+// const moment = require('moment')
 const simpleGit = require('simple-git')
 const FILE_PATH = './data.json'
 
-const date = moment().format()
+// const date = moment().format()
+// format --> YYYY-MM-DD
+const date = [
+    "2021-04-03T18:44:38+05:30",
+    "2021-04-04T18:44:38+05:30",
+    "2021-04-05T18:44:38+05:30",
+    "2021-04-06T18:44:38+05:30",
+    "2021-04-07T18:44:38+05:30",
+    "2021-04-08T18:44:38+05:30",
+    //and so on....
+]
 
-const payload = {
-    date
-}
 
-jsonfile.writeFile(FILE_PATH, payload, () => {
-    //simpleGit().add(FILE_PATH).commit('bot69 working', {'--date':date}).push()
-})
-
-simpleGit().add('./index.js').commit('index.js commit', {'--date':date}).push()
+    const payload = {
+        date
+    }
+    jsonfile.writeFile(FILE_PATH, payload, () => {
+        console.log(date)
+        simpleGit().add(FILE_PATH).commit('bot69 working', {'--date':date})
+    })
+simpleGit().push()
+//simpleGit().add('./index.js').commit('index.js commit', {'--date':date}).push()
 
